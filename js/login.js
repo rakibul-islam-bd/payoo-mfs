a5fb8e2c5422683d708b07b8a260b96f124283aa
Md.R.I.Rakib <r.i.rakibbd22@gmail.com>
2025-02-22 22:47:44 +0600

    event handelar

---
+++ js/login.js
@@ -0,0 +1,5 @@
+document.getElementById('btn-login').addEventListener('click', function (event){
+ event.preventDefault();
+ const phoneNumber = document.getElementById('phone-number').value;
+ console.log(phoneNumber)
+})
